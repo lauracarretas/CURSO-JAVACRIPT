00412e32d27743107134efafeca8308eceaf2c9b
lauracarretas <125823602+lauracarretas@users.noreply.github.com>
2023-03-28 14:36:40 +0200

    Add files via upload

---
+++ EJERCICIO-2/Listas.js
@@ -0,0 +1,15 @@
+
+const Lista = {
+    nombre: "Laura Carretas",
+    edad: 34,
+    isDevelopper: true,
+    fecha_valores: new Date(1989, 0, 26),
+    libro_favorito: {
+        titulo: "Lejos de Luisiana",
+        autor: "Luz Gabás",
+        fecha: new Date(2022, 10, 3),
+        url: "https://www.amazon.es/Lejos-Luisiana-Planeta-Espa%C3%B1oles-Iberoamericanos/dp/8408265601/ref=sr_1_1?adgrpid=139284878621&hvadid=628153057878&hvdev=c&hvlocphy=1005493&hvnetw=g&hvqmt=e&hvrand=8702922074800535210&hvtargid=kwd-1851942565881&hydadcr=13931_2286510&keywords=lejos+de+luisiana&qid=1680006666&sr=8-1"
+    }
+}
+
+console.log(Lista);
