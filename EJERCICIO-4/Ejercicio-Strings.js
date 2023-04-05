9b2c988887bf43a59cfe6ced65234fcd6cbcdd60
lauracarretas <125823602+lauracarretas@users.noreply.github.com>
2023-04-05 20:52:37 +0200

    Add files via upload

---
+++ EJERCICIO-4/Ejercicio-Strings.js
@@ -0,0 +1,29 @@
+
+let nombre = "Laura"
+let apellidos = "Carretas Perulero"
+let estudiante = `${nombre} ${apellidos}`
+console.log(estudiante)
+
+let estudianteMayus = `${estudiante}`
+console.log(estudianteMayus.toUpperCase())
+
+let estudianteMinus = `${estudiante}`
+console.log(estudianteMinus.toLowerCase())
+
+let cantidad_letras = estudiante.length
+console.log(estudiante.length)
+console.log(cantidad_letras)
+
+let primeraletra = `${estudiante}`
+console.log(estudiante[0])
+
+let ultimaletra = `${estudiante}`.endsWith
+console.log(estudiante.endsWith('o'))
+
+
+console.log(estudiante.trim().length)
+
+let inluye = estudiante.includes
+console.log(estudiante.includes('Laura'))
+
+
