436354198b6fd309e3ae7a0f5f1ba8ed5ede4be5
lauracarretas <125823602+lauracarretas@users.noreply.github.com>
2023-04-17 20:13:24 +0200

    Add files via upload

---
+++ EJERCICIO-5/Ejercicio-numbers .js
@@ -0,0 +1,23 @@
+
+let altura_cm = 162;
+console.log(parseInt(altura_cm));
+
+
+let altura_m = 1.62;
+console.log(parseFloat(altura_m));
+
+
+let peso_kg = 60.5;
+console.log(parseFloat(peso_kg));
+
+
+let altura = Math.ceil(altura_m);
+console.log(altura);
+
+
+let peso = Math.floor(peso_kg);
+console.log(peso);
+
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(iguales);
+
